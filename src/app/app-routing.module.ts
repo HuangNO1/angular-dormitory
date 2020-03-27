5e3dd7ded51bba7fac343ee3adc34a70bfbb4660
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-03-27 13:33:09 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignComponent } from './view/sign/sign.component';
+import { WorkComponent } from './view/work/work.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "sign",
+    component: SignComponent
+  },
+  {
+    path: "work",
+    component: WorkComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
